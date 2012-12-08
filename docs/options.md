43b30b58cc064529e6c70fd0cb97d501e9eb8d06
Chris Talkington <chris@talkingtontech.com>
2012-12-07 18:41:27 -0600

    cleanup. add force option.
    should now be ready for grunt v0.4 release.

--- docs/options.md
+++
@@ -1,3 +0,0 @@
-##### src ```string```
-
-This defines what paths this task will clean recursively (supports [grunt.template](https://github.com/cowboy/grunt/blob/master/docs/api_template.md) and [minimatch](https://github.com/isaacs/minimatch)).
--- tasks/clean.js
+++ tasks/clean.js
@@ -12,15 +12,18 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('clean', 'Clean files and folders.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options();
+    var options = this.options({
+      force: false
+    });
 
     grunt.verbose.writeflags(options, 'Options');
 
     // Clean specified files / dirs.
     this.file.src.forEach(function(filepath) {
       grunt.log.write('Cleaning "' + filepath + '"...');
+
       try {
-        grunt.file.delete(filepath);
+        grunt.file.delete(filepath, options);
         grunt.log.ok();
       } catch (e) {
         grunt.log.error();
