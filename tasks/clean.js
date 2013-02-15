a7d7b0ecf656c8515a1e0ceef70478e0c9dc9cab
Tyler Kellen <tyler@sleekcode.net>
2013-02-15 17:19:16 -0500

    revert to 0.3.1

--- tasks/clean.js
+++ tasks/clean.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask("clean", "Clear files and folders", function() {
 
-    var helpers = require('grunt-contrib-lib').init(grunt);
+    var helpers = require('grunt-lib-contrib').init(grunt);
     var options = helpers.options(this);
 
     grunt.verbose.writeflags(options, "Options");
