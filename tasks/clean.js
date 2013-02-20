3af9c0ee7e5777e8902d8589df844713261103d2
Pierre Voisin <pierre.voisin@gmail.com>
2013-02-19 22:36:29 -0500

    Introducing "deleteEmptyFolders" option to let folders be deleted after they become empty.

--- tasks/clean.js
+++ tasks/clean.js
@@ -9,11 +9,14 @@
 'use strict';
 
 module.exports = function(grunt) {
+  var fs = require('fs');
+  var path = require('path');
 
   grunt.registerMultiTask('clean', 'Clean files and folders.', function() {
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      force: false
+      force: false,
+      deleteEmptyFolders: false
     });
 
     grunt.verbose.writeflags(options, 'Options');
@@ -25,6 +28,17 @@ module.exports = function(grunt) {
       try {
         grunt.file.delete(filepath, options);
         grunt.log.ok();
+
+        if(options.deleteEmptyFolders) {
+            var folder = path.dirname(filepath);
+            while((options.force || grunt.file.isPathInCwd(folder)) && !fs.readdirSync(folder).length) {
+                grunt.log.write('Cleaning empty folder "' + folder + '"...');
+                grunt.file.delete(folder, options);
+                grunt.log.ok();
+                folder = path.dirname(folder);
+            }
+        }
+
       } catch (e) {
         grunt.log.error();
         grunt.verbose.error(e);
